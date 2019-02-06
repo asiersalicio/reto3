225f718ff70aa70612fbb6a19576e747abbf7969
unknown <Elorrieta Errekamari@INFP4A101-12.H015112.NET>
2019-02-06 12:53:45 +0100

    Devoluciones funcionando

---
+++ src/main/java/ControladoresPaneles/ControladorCambioFinal.java
@@ -0,0 +1,15 @@
+package ControladoresPaneles;
+
+import Vista.PaneCambioFinal;
+import Vista.PanePago;
+
+
+public class ControladorCambioFinal {
+		public PaneCambioFinal paneCambioFinal;
+		public ControladorCambioFinal controladorCambioFinal;
+	public ControladorCambioFinal(PaneCambioFinal paneCambioFinal, PanePago panePago)
+	{
+		controladorCambioFinal=this;
+		this.paneCambioFinal=paneCambioFinal;
+	}
+}
