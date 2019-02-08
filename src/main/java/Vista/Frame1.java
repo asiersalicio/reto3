2013fa2bedfdb0770aee21efaa8e2fd132b103db
unknown <Elorrieta Errekamari@INFP4A101-12.H015112.NET>
2019-02-08 13:01:23 +0100

    Titulo del frame y preparando para mergear a master

--- src/main/java/Vista/Frame1.java
+++ src/main/java/Vista/Frame1.java
@@ -5,6 +5,10 @@ import java.awt.event.WindowEvent;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import java.awt.Toolkit;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Window.Type;
 
 public class Frame1 {
 	public JFrame frame1;
@@ -14,7 +18,11 @@ public class Frame1 {
 	public Frame1()
 	{
 		frame1 = new JFrame();
-		frame1.setTitle("Ticket vending");
+		frame1.setType(Type.UTILITY);
+		frame1.setFont(new Font("Arial Rounded MT Bold", Font.ITALIC, 12));
+		frame1.setForeground(new Color(0, 51, 102));
+		frame1.setIconImage(Toolkit.getDefaultToolkit().getImage(Frame1.class.getResource("/ImagenesVista/termiLOGOpeke.png")));
+		frame1.setTitle("TermibBus Ticket Station v1.0 by ASAlba Solutions");
 		frame1.setBounds(0, 0, 800, 600);		
 		
 		frame1.addWindowListener(new WindowAdapter() {
